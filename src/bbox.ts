f19b215d8a33227597d5ab194771d364adc085a3
M Brammall <mibrammall.github@gmail.com>
2022-02-23 21:52:36 +0000

    Fix bbox

--- src/bbox.ts
+++ src/bbox.ts
@@ -1,8 +1,13 @@
-export default class Bbox {
+type Point = [x: number, y: number];
+export class Bbox {
+  minX: number;
+  maxX: number;
+  minY: number;
+  maxY: number;
   // a list of points [x, y]
-  constructor(arr) {
-    const xs = arr.map(val => val[0]);
-    const ys = arr.map(val => val[1]);
+  constructor(arr: Point[]) {
+    const xs = arr.map((val) => val[0]);
+    const ys = arr.map((val) => val[1]);
     this.minX = Math.min.apply(null, xs);
     this.maxX = Math.max.apply(null, xs);
     this.minY = Math.min.apply(null, ys);
@@ -21,10 +26,16 @@ export default class Bbox {
     return [(this.minX + this.maxX) * 0.5, (this.minY + this.maxY) * 0.5];
   }
 
-  intersect({minX, maxX, minY, maxY}) {
-    const bbox = new Bbox([[minX, minY], [maxX, maxY]]);
-    return Math.abs(this.center[0] - bbox.center[0]) < (this.width + bbox.width) * 0.5 &&
-      Math.abs(this.center[1] - bbox.center[1]) < (this.height + bbox.height) * 0.5;
+  intersect({ minX, maxX, minY, maxY }: Bbox) {
+    const bbox = new Bbox([
+      [minX, minY],
+      [maxX, maxY],
+    ]);
+    return (
+      Math.abs(this.center[0] - bbox.center[0]) <
+        (this.width + bbox.width) * 0.5 &&
+      Math.abs(this.center[1] - bbox.center[1]) <
+        (this.height + bbox.height) * 0.5
+    );
   }
-
 }
